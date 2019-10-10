35dd0b7579d84af8996dd719671cf1a1c4ff9c5a
Harrison Louis Greeves <hgreeves@protonmail.com>
2019-10-10 14:03:47 +0800

    Controller, route, and resource has been added. Data has been formatted accordingly and can now be retrieved.

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Employee; // connects to the model
+use App\Http\Resources\Employee as EmployeeResource; // connects to the resource file  
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $employees = Employee::paginate(10);
+
+        return EmployeeResource::collection($employees);
+    }
+}
